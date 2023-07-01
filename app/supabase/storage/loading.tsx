b2829f11ee08dc86eb69793899428e072db5b8ca
saikise <134133636+saikise@users.noreply.github.com>
2023-07-01 14:18:28 +0800

    add loading skeleton for server component pages

---
+++ app/supabase/storage/loading.tsx
@@ -0,0 +1,8 @@
+// References:
+// - https://nextjs.org/docs/app/building-your-application/routing/loading-ui-and-streaming
+// - https://flowbite.com/docs/components/skeleton/
+import LoadingSkeleton from "@/components/server-components/LoadingSkeleton";
+
+export default function Loading() {
+  return <LoadingSkeleton />;
+}
