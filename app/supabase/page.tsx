6ab9929abbc8bc339a99e6438c979293419fcd6a
saikise <134133636+saikise@users.noreply.github.com>
2023-07-03 13:51:56 +0800

    add comment to retrigger Vercel deployment

--- app/supabase/page.tsx
+++ app/supabase/page.tsx
@@ -1,3 +1,4 @@
+// This page has no content yet so just redirect to the first Supabase page
 import { redirect } from "next/navigation";
 export default function Supabase() {
   redirect("/supabase/authentication");
--- app/vercel/page.tsx
+++ app/vercel/page.tsx
@@ -1,3 +1,4 @@
+// This page has no content yet so just redirect to the first Vercel page
 import { redirect } from "next/navigation";
 export default function Vercel() {
   redirect("/vercel/serverless-functions");
